088c4b439842a9e2e35a0969001fbd5d28e272f4
Ben <ben.c.hawker@gmail.com>
2016-10-30 09:16:54 +0800

    Add support for multiple promos of the same type

--- lib/promotion_calculator.rb
+++ lib/promotion_calculator.rb
@@ -10,35 +10,38 @@ class PromotionCalculator
   end
 
   def calculate
-    product_specific_discount + basket_total_specific_discount
+    product_specific_discounts + basket_total_specific_discounts
   end
 
   private
 
-  def product_specific_discount
-    product_promos = PROMOTIONS["product_multiples_greater_or_equal_to"]
-    product_code = product_promos["product_code"]
+  def product_specific_discounts
+    discount = 0
 
-    if basket_counts[product_code] >= product_promos["condition"]
-      product_promos["discount"] * basket_counts[1]
-    else
-      0
+    PROMOTIONS["product_multiples_greater_or_equal_to"].each do |promo|
+      product_count = basket_counts.fetch(promo["product_code"], 0)
+
+      if product_count >= promo["condition"]
+        discount += promo["discount"] * product_count
+      end
     end
+    discount
   end
 
-  def basket_total_specific_discount
-    total_less_product_discount = total - product_specific_discount
-    basket_promos = PROMOTIONS["basket_total_greater_than"]
+  def basket_total_specific_discounts
+    discount = 0
+    total_less_product_discount = total - product_specific_discounts
 
-    if total_less_product_discount > basket_promos["condition"]
-      total_less_product_discount / basket_promos["discount"]
-    else
-      0
+    PROMOTIONS["basket_total_greater_than"].each do |promo|
+      if total_less_product_discount > promo["condition"]
+        discount += total_less_product_discount / promo["discount"]
+      end
     end
+    discount
   end
 
   def basket_counts
-    counts = basket.group_by{ |item| item }.map{|k,v| [k, v.count] }
+    counts = basket.group_by{ |item| item }.map{ |k,v| [k, v.count] }
     counts = Hash[*counts.flatten]
   end
 end
